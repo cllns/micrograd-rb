8f94f11b3d1d6739f5501c23893189e3fae10896
Sean Collins <sean@cllns.com>
2025-02-26 15:12:50 -0700

    Initial commit

---
+++ spec/rbgrad_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Micrograd do
+  it "has a version number" do
+    expect(Micrograd::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
