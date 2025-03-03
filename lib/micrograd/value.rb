ff00160a8937f0dc821f8b8ac938d3764afadea8
Sean Collins <sean@cllns.com>
2025-03-03 11:18:46 -0700

    Use goodcop's base style guide

--- lib/micrograd/value.rb
+++ lib/micrograd/value.rb
@@ -4,10 +4,11 @@ module Micrograd
   class Value
     attr_reader :data, :label, :grad, :operation, :previous
 
-    def initialize(data:, label:, operation: nil, previous: [])
+    def initialize(data:, label:, backward: -> {}, operation: nil, previous: [])
       @data = data
       @label = label
       @grad = nil
+      @backward = backward
       @operation = operation
       @previous = previous.to_set
     end
@@ -23,6 +24,10 @@ module Micrograd
       Value.new(
         data: data + other.data,
         label: "#{label}+#{other.label}".to_sym,
+        backward: lambda {
+          self.grad = 1.0
+          other.grad = 1.0
+        },
         operation: :+,
         previous: [self, other]
       )
@@ -32,18 +37,24 @@ module Micrograd
       Value.new(
         data: data * other.data,
         label: "#{label}*#{other.label}".to_sym,
+        backward: lambda {
+          self.grad = other.data * out.grad,
+                      other.grad = data * out.grad
+        },
         operation: :*,
         previous: [self, other]
       )
     end
 
     def tanh
+      t = (Math.exp(2 * data) - 1) / (Math.exp(2 * data) + 1)
+      # Other valid options
+      # Math.tanh(data)
+      # (Math.exp(data) - Math.exp(-data)) / (Math.exp(data) + Math.exp(-data))
       Value.new(
-        data: (Math.exp(2 * data) - 1) / (Math.exp(2 * data) + 1),
-        # Other valid options
-        # data: Math.tanh(data),
-        # data: (Math.exp(data) - Math.exp(-data)) / (Math.exp(data) + Math.exp(-data)),
+        data: t,
         label: "tanh(#{label})".to_sym,
+        backward: -> { self.grad = (1 - t**2) * out.grad },
         operation: :tanh,
         previous: [self]
       )
