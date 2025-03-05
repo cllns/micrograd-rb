d7461a8a1c4ba1f3d24122ea6d02fdfd3569b31f
Sean Collins <sean@cllns.com>
2025-03-05 13:13:36 -0700

    Add (no expectation) example for loss function of tiny dataset
    Just a way to track this example dataset and see its output while working through this

--- spec/mlp_spec.rb
+++ spec/mlp_spec.rb
@@ -14,4 +14,31 @@ RSpec.describe Micrograd::MLP do
     mlp = Micrograd::MLP.new(3, [4, 4, 1])
     expect(mlp.layers).to_not be_empty
   end
+
+  describe "a tiny dataset" do
+    # Example from: https://youtu.be/VMj-3S1tku0?feature=shared&t=6664
+
+    it do
+      mlp = Micrograd::MLP.new(3, [4, 4, 1])
+
+      inputs = [
+        [2.0, 3.0, -1.0],
+        [3.0, -1.0, 0.5],
+        [0.5, 1.0, 1.0],
+        [1.0, 1.0, -1.0],
+      ]
+
+      targets = [1.0, -1.0, -1.0, 1.0]
+
+      outputs = inputs.map { |input| mlp.call(input) }
+
+      loss = targets.zip(outputs).map do |target, output|
+        (output - target) ** 2
+      end.sum
+
+      loss.backward
+
+      p mlp.layers.first.neurons.first.weights.first.grad
+    end
+  end
 end
